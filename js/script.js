5e403a605426c02cd92f617165c58276a048c443
loui59f1 <loui59f1@stud.kea.dk>
2020-10-10 11:48:37 +0200

    Responsive tilpasninger

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-// Venter på alt indhold er loadet --> funktion loadJSON
-document.addEventListener("DOMContentLoaded", loadJSON);
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+	console.log(sidenVises);
+
+	document.querySelector("#closebtn").addEventListener("click", closeNav);
+	document.querySelector("#openbtn").addEventListener("click", openNav);
+
+}
+
+function openNav() {
+	document.getElementById("myNav").style.width = "80%";
+}
+
+function closeNav() {
+	document.getElementById("myNav").style.width = "0%";
+}
